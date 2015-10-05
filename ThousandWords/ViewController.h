39f3064a6172f5b7c3bf2e687ca943a9089c72b4
Dustin Mahone <victorys.mine@gmail.com>
2015-10-05 10:22:48 -0400

    Initial Commit

---
+++ ThousandWords/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThousandWords
+//
+//  Created by Dustin M on 10/5/15.
+//  Copyright © 2015 Vento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThousandWords/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ThousandWords
+//
+//  Created by Dustin M on 10/5/15.
+//  Copyright © 2015 Vento. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
